b4f7e2517d1d2b09926d5a0f0d314049dcdd4b43
zegfarce <zegfarce@protonmail.com>
2022-06-14 20:35:28 -0500

    Partially revert "Revert "Redo how static file serving is done, clean up static dir.", other additions."
    This partially reverts commit 48484f6459b213b2c3fafef3a6f1e24fb99b0fc6.
    Specifically, this reverts the revert, but keeps the other changes.
    
    adding things to the same commit as a revert is pain for revert reverts
    also please don't revert random shit and push without asking, okay pk?

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,25 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import { settings } from "../settings.ts";
 
-// Oak can't handle dynamic routing alongside sending static content over.
-// Originally, the plan was to have one `static` directory, which would send
-// its content over if the `/m/` parameter was matched.
-// Instead I have to include the `static` directory in another directory
-// called `m` in order to get the results I want without directory traversal
-// coming into play.
-
-// I swear to god if I ever see the fucking guy who made Oak in person I'm going to
-// [ SECTION EXPUNGED TO PREVENT LEGAL ACTION TAKEN AGAINST US ]
-
 let app = new Application();
+let router = new Router();
 
-app.use(async function (ctx, next) {
+// Uses a somewhat obscure feature of path-to-regexp; it supports capture groups.
+// See <https://github.com/pillarjs/path-to-regexp#unnamed-parameters>
+router.get("/m/(.*)", async function (ctx) {
   try {
+    // send the file at path ctx.params[0], relative to root settings.staticFileDir
     await ctx.send({
+      path: `${ctx.params[0]}`,
       root: `${settings.staticFileDir}`,
     });
   } catch {
-    await next();
+    ctx.response.status = 404;
   }
 });
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.addEventListener("listen", (evt) => {
   let protocol = (evt.secure ? "https" : "http");
@@ -31,4 +29,4 @@ app.addEventListener("listen", (evt) => {
   console.log(`Listening on: ${protocol}://${hostname}:${port}`);
 });
 
-app.listen({ port: settings.sitePort });
+await app.listen({ port: settings.sitePort });
