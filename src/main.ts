0888cebd1725ea4c1236a727c0eb2ce7a07e8b5a
PK <prettykool@protonmail.com>
2022-07-01 20:33:01 -0500

    Removed unused thing

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application } from "https://deno.land/x/oak/mod.ts";
 import { settings } from "../settings.ts";
 import { auth } from "./auth.ts";
 import { media } from "./static.ts";
