b9eec0bf06459c76f1d3601b761742e526e71cb0
PK <prettykool@protonmail.com>
2022-07-04 21:33:50 -0500

    Add test for flagging

--- test/torrent.ts
+++ test/torrent.ts
@@ -119,6 +119,22 @@ Deno.test("Like Torrent", async () => {
   assertNotEquals(res.err, true);
 });
 
+Deno.test("Flag Torrent", async () => {
+  const r = await fetch(torrentURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${userJWT}`,
+    },
+    body: JSON.stringify({
+      "type": "Flag",
+    }),
+  });
+
+  const res = await r.json();
+  assertNotEquals(res.err, true);
+});
+
 Deno.test("Try to like Torrent twice", async () => {
   const r = await fetch(torrentURL, {
     method: "POST",
