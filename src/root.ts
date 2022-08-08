066580d69b9af6607200817962542abcb5c75c19
PK <prettykool@protonmail.com>
2022-08-08 17:03:53 -0500

    `deno fmt`

--- src/root.ts
+++ src/root.ts
@@ -9,7 +9,7 @@ root.get("/", async function (ctx: Content) {
   const u = new URL(settings.siteURL);
   const userCount = await tableCount("users");
   const torrentCount = await tableCount("torrents");
-    const listCount = await tableCount("lists");
+  const listCount = await tableCount("lists");
   const commentCount = await tableCount("comments");
 
   ctx.response.body = {
